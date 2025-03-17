9fdcf11a880b5306ab7ee32057a29ee07356b67e
cuno902 <cuongphlo902@gmail.com>
2025-03-17 23:11:21 +0700

    final

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express from "express"
 import { connectDb } from "./config/db.js";
 import cors from 'cors';
 import userRoutes from "./routes/userRoutes.js";
